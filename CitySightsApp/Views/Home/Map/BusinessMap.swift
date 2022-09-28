47fcd13400a165523117cf8078cbb70ff1ff0453
Fidan Oruc <fidanorucc>
2022-09-28 10:37:58 +0200

    - Add Coordinator class, mapView(viewFor:) function, makeCoordinator() function in BusinessMap

--- CitySightsApp/Views/Home/Map/BusinessMap.swift
+++ CitySightsApp/Views/Home/Map/BusinessMap.swift
@@ -35,7 +35,7 @@ struct BusinessMap: UIViewRepresentable{
     func makeUIView(context: Context) -> MKMapView {
         
         let mapView = MKMapView()
-        
+        mapView.delegate = context.coordinator
         // Make the user show up on the map
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .followWithHeading
@@ -62,4 +62,50 @@ struct BusinessMap: UIViewRepresentable{
         // if we dont need mapview anymore
         uiView.removeAnnotations(uiView.annotations)
     }
+    
+    // MARK: - Coordinator class
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate{
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            // if the annotation is the user blue dot(current location), return nil
+            if annotation is MKUserLocation{
+                return nil
+            }
+            
+            // Check if there is a reusable annotation view first
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Constants.annotationReuseId)
+            
+            if annotationView == nil{ // we could not get a reusable one
+                
+                // create a new one
+                // Create an annotation view
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: Constants.annotationReuseId)
+                
+                annotationView!.canShowCallout = true
+                annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            }else{
+                
+                // We got a reusable one
+                annotationView!.annotation = annotation
+            }
+            
+            // return it
+            return annotationView
+        }
+    }
 }
+/*
+ 
+ How do you add annotations to a MKMapView?
+ Annotations can be directly added using MKPointAnnotation and specifying coordinates
+ 
+ How does the protocol delegate pattern help handle events for a MapView?
+ The protocol delegate pattern is a commonly used pattern when working with UIKit, although less often in SwiftUI. We can call methods in the delegate to run when an event is detected, and the delegate can also be notified when an event occurs.
+
+
+ */
