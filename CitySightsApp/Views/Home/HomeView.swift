1a6721c62c47a7b1109657b8288ef3e750079c96
Fidan Oruc <fidanorucc>
2022-10-12 11:26:42 +0200

    - Add Rectangle overlay for the map in Homeview

--- CitySightsApp/Views/Home/HomeView.swift
+++ CitySightsApp/Views/Home/HomeView.swift
@@ -28,7 +28,7 @@ struct HomeView: View {
                             Image(systemName: "mappin")
                             Text("San Francisco")
                             Spacer()
-                            Button("Switch to the map view") {
+                            Button("Switch to the Map view") {
                                 self.isMapShowing = true
                             }
                             
@@ -40,15 +40,34 @@ struct HomeView: View {
                     .navigationBarHidden(true)
                     
                 }else{
-                    // show map
-                    BusinessMap(selectedBusiness: $selectedBusiness)
-                        .ignoresSafeArea()
-                        .sheet(item: $selectedBusiness) { business in
-                            
-                            // Create a business detail view instance
-                            // Pass in the selected business
-                            BusinessDetail(business: business)
-                        }
+                    // Show map
+                    ZStack(alignment: .top){
+                        BusinessMap(selectedBusiness: $selectedBusiness)
+                            .ignoresSafeArea()
+                            .sheet(item: $selectedBusiness) { business in
+                                
+                                // Create a business detail view instance
+                                // Pass in the selected business
+                                BusinessDetail(business: business)
+                            }
+                        // Rectangle overlay
+                        ZStack{
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .cornerRadius(5)
+                                .frame(height: 48)
+                            HStack{
+                                Image(systemName: "mappin")
+                                Text("San Francisco")
+                                Spacer()
+                                Button("Switch to the List view") {
+                                    self.isMapShowing = false
+                                }
+                            }.padding()
+                        }.padding()
+                    }
+                    .navigationBarHidden(true)// navigation title is specified for the child view of the navigationview. when run the app skip the if statement and first child of the navigation view is zstack
+                    
                 }
             }
             
@@ -68,3 +87,8 @@ struct HomeView_Previews: PreviewProvider {
         HomeView()
     }
 }
+/*
+ What does the Binding property do when passed as a variable to a view?
+ It acts as a two way binding to the State variable in the parent view, so that the child view can modify the variable
+ Bindings are good to use when a child view needs to track/modify a variable from a parent view
+*/
